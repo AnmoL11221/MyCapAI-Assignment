0ea9a1f20c909bf2a615dfcd1d23525bb174a0db
AnmoL11221 <97684962+AnmoL11221@users.noreply.github.com>
2024-06-18 23:16:50 +0530

    Create positive_list.py

---
+++ positive_list.py
@@ -0,0 +1,12 @@
+list1 = [12, -7, 5, 64, -14]
+list_1 = []
+for i in (list1):
+  if i>0:
+    list_1.append(i)
+print(list_1)
+list2 = [12, 14, -95, 3]
+list_2 = []
+for j in (list2):
+  if j>0:
+    list_2.append(j)
+print(list_2)
